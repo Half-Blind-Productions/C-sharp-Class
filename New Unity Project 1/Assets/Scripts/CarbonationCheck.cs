e49b8f1110ffe81113e8c0bc091b19739fc2e145
Wesley Messer <westhehedgehog16@gmail.com>
2016-01-27 09:11:40 -0700

    Made this for my if Statement assignment which was not nearly as bad as I thought it would be.

---
+++ New Unity Project 1/Assets/Scripts/CarbonationCheck.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarbonationCheck : MonoBehaviour
+{
+	float CarbonationLevel = 100.0f;
+	float StiffLimitCarbonation = 75.0f;
+	float FlatLimitCarbonation = 49.0f;
+	
+	
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			CarbonatonTest();
+		
+		CarbonationLevel -= Time.deltaTime * 4f;
+	}
+	
+	
+	void CarbonatonTest ()
+	{
+		// If the carbonation level is greater than StiffLimitCarbonation
+		if(CarbonationLevel > StiffLimitCarbonation)
+		{
+			// ... do this.
+			print("Soda is too stiff.");
+		}
+		// If The soda is not too stiff but below FlatLimitCarbonation
+		else if(CarbonationLevel < FlatLimitCarbonation)
+		{
+			// ... do this.
+			print("Soda is too flat.");
+		}
+		// If it's inbetween the Stiff level and Flat level
+		else
+		{
+			// ... do this.
+			print("Soda is good. <(^0^)>");
+		}
+	}
+}
